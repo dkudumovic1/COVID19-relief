18a1c5493fa60b8fd64389bfc782a3ba33b97234
dkudumovic1 <56407202+dkudumovic1@users.noreply.github.com>
2021-03-26 15:28:52 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HackAtHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Korisnik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackAtHome.Models
+{
+    public class Korisnik
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+    }
+}
