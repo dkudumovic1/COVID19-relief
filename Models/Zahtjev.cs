b6caffd8b0e591c1f0c686d71633e46dc5de1af7
Dzenana <dhuseinspa1@etf.unsa.ba>
2021-03-27 12:57:11 +0100

    Update autorizacije za index korisnik i volonter

--- Models/Zahtjev.cs
+++ Models/Zahtjev.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,13 +12,16 @@ namespace HackAtHome.Models
 
         public int KorisnikId { get; set; }
         public bool Tutor { get; set; }
+        [Display(Name = "Medicinska pomoć")]
         public bool MedicinskaPomoc { get; set; }
         public bool Vakcinisan { get; set; }
         public bool Dostava { get; set; }
+        [Display(Name = "Znakovni jezik")]
         public bool ZnakovniJezik { get; set; }
         public string Opis { get; set; }
 
         public string CreatedByUserId { get; set; }
+        [Display(Name = "Vrijeme kreiranja zahtjeva")]
 
         public DateTime? CreatedDateTime { get; set; }
 
